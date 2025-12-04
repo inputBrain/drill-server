68311e410949932e5269e88bdcd66dce62fe5482
Oleksandr Voznyi <vsanyinclude@gmail.com>
2025-12-04 21:53:44 +0200

    hot fix 2

--- Drill.Server.Host/Controllers/Client/AbstractClientController.cs
+++ Drill.Server.Host/Controllers/Client/AbstractClientController.cs
@@ -6,7 +6,7 @@ namespace Drill.Server.Host.Controllers.Client
     [ApiController]
     [ApiExplorerSettings(GroupName = "v1")]
     [Produces("application/json")]
-    [Route("[controller]/[action]")]
+    [Route("api/api/[controller]/[action]")]
     public abstract class AbstractClientController<T> : AbstractBaseController<T>
     {
         protected AbstractClientController(ILogger<T> logger, IDatabaseContainer databaseContainer) : base(logger, databaseContainer)
